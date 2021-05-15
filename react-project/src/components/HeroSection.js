d11a77de1b74a6717167fb5a3d806cc2e8f8d50d
Laura Gwen <lauraburch.lb@gmail.com>
2021-05-15 13:52:40 -0700

    fixed a missplaced div

--- react-project/src/components/HeroSection.js
+++ react-project/src/components/HeroSection.js
@@ -9,21 +9,22 @@ function HeroSection() {
       <video src="/videos/video-2.mp4" autoPlay loop muted />
       <h1>ADVENTURRE AWAITS</h1>
       <p>What are you waiting for?</p>
-      <div className="her0-btns"></div>
-      <Button
-        className="btns"
-        buttonStyle="btn--outline"
-        buttonSize="btn--large"
-      >
-        GET STARTED
-      </Button>
-      <Button
-        className="btns"
-        buttonStyle="btn--primary"
-        buttonSize="btn--large"
-      >
-        WATCH TRAILER <i className="far fa-play-circle"></i>
-      </Button>
+      <div className="hero-btns">
+        <Button
+          className="btn"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btn"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH TRAILER <i className="far fa-play-circle"></i>
+        </Button>
+      </div>
     </div>
   );
 }
