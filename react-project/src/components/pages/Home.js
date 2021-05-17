cf55f873a7e08e9858a37fb704e33017591be7ab
Laura Gwen <lauraburch.lb@gmail.com>
2021-05-17 12:09:00 -0700

    teemp fix for photos to render on pages

--- react-project/src/components/pages/Home.js
+++ react-project/src/components/pages/Home.js
@@ -1,7 +1,7 @@
-import "../../App.css";
-import HeroSection from "../HeroSection";
 import React from "react";
+import "../../App.css";
 import Cards from "../Cards";
+import HeroSection from "../HeroSection";
 
 function Home() {
   return (
